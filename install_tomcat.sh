8f03f84d27c01897e200cbc34f1c58d84383be12
akranthi113 <150571677+akranthi113@users.noreply.github.com>
2024-02-12 21:29:09 +0530

    Update install_tomcat.sh

--- install_tomcat.sh
+++ install_tomcat.sh
@@ -1,43 +1,28 @@
-
 #!/bin/bash
 
-# Update package index
-sudo yum update -y
-
-# Install Java OpenJDK 1.8 development kit
+# Install Java 1.8 JDK
 sudo yum install -y java-1.8.0-openjdk-devel.x86_64
 
 # Download Tomcat latest version (adjust the version number if needed)
-TOMCAT_VERSION=9.0.85
-TOMCAT_DOWNLOAD_URL="https://dlcdn.apache.org/tomcat/tomcat-9/v${TOMCAT_VERSION}/bin/apache-tomcat-${TOMCAT_VERSION}.tar.gz"
-wget -P /tmp "${TOMCAT_DOWNLOAD_URL}"
+TOMCAT_VERSION=$(curl -s https://archive.apache.org/dist/tomcat/ | grep -oP '(?<=href="v)([0-9]+\.[0-9]+\.[0-9]+)/' | tail -1 | sed 's/\///g')
+TOMCAT_DOWNLOAD_URL="https://archive.apache.org/dist/tomcat/tomcat-${TOMCAT_VERSION}/bin/apache-tomcat-${TOMCAT_VERSION}.tar.gz"
+wget -P /home/ec2-user "${TOMCAT_DOWNLOAD_URL}"
 
-# Extract Tomcat archive
-sudo tar xf /tmp/apache-tomcat-${TOMCAT_VERSION}.tar.gz -C /opt
+# Extract Tomcat archive with verbose output
+sudo tar -xvf /home/ec2-user/apache-tomcat-${TOMCAT_VERSION}.tar.gz -C /home/ec2-user
 
-# Create a symbolic link
-sudo ln -s /opt/apache-tomcat-${TOMCAT_VERSION} /opt/tomcat
-
-# Configure environment variables
-echo "export CATALINA_HOME=\"/opt/tomcat\"" | sudo tee -a /etc/profile.d/tomcat.sh
-echo "export PATH=\"\$CATALINA_HOME/bin:\$PATH\"" | sudo tee -a /etc/profile.d/tomcat.sh
-
-# Reload environment variables
-source /etc/profile.d/tomcat.sh
-
-# Remove <Valve> and <Allow> tags from context.xml
-sudo sed -i '/<Valve/d' /opt/tomcat/conf/context.xml
-sudo sed -i '/<Allow>/d' /opt/tomcat/conf/context.xml
+# Remove lines containing <Valve> with specific patterns from context.xml
+sudo sed -i '/<Valve className="org.apache.catalina.valves.RemoteAddrValve" allow="127\.[0-9]\+\.[0-9]\+\.[0-9]\+|::1|0:0:0:0:0:0:0:1" \/>/d' /home/ec2-user/apache-tomcat-${TOMCAT_VERSION}/webapps/manager/META-INF/context.xml
 
 # Add roles and users to tomcat-users.xml
-sudo tee -a /opt/tomcat/conf/tomcat-users.xml > /dev/null <<EOT
+sudo tee -a /home/ec2-user/apache-tomcat-${TOMCAT_VERSION}/conf/tomcat-users.xml > /dev/null <<EOT
 <role rolename="manager-gui"/>
 <role rolename="manager-script"/>
 <user username="tomcat" password="tomcat" roles="manager-gui,manager-script"/>
 EOT
 
 # Start Tomcat
-sudo /opt/tomcat/bin/startup.sh
+/home/ec2-user/apache-tomcat-${TOMCAT_VERSION}/bin/startup.sh
 
 # Display Tomcat version
 echo "Apache Tomcat ${TOMCAT_VERSION} has been installed and started successfully."
