1f0bbc73631ff1d9108768309006979e42568002
akranthi113 <150571677+akranthi113@users.noreply.github.com>
2024-02-12 20:40:22 +0530

    Update installjenkins.sh

--- installjenkins.sh
+++ installjenkins.sh
@@ -1,43 +1,43 @@
 #!/bin/bash
 
 # Install Java OpenJDK 11
-sudo amazon-linux-extras install java-openjdk11 -y
+sudo amazon-linux-extras install java-openjdk11 -y && \
 
 # Download Jenkins repository configuration
-sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo && \
 
 # Import Jenkins repository key
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key && \
 
 # Install Jenkins
-sudo yum install jenkins -y
+sudo yum install jenkins -y && \
 
 # Display Jenkins version
-jenkins --version
+jenkins --version && \
 
 # Check Jenkins service status
-service jenkins status
+service jenkins status && \
 
 # Enable Jenkins service
-sudo systemctl enable jenkins
+sudo systemctl enable jenkins && \
 
 # Start Jenkins service
-sudo systemctl start jenkins
+sudo systemctl start jenkins && \
 
 # Install Git
-sudo yum install -y git
+sudo yum install -y git && \
 
 # Download Apache Maven repository configuration
-sudo wget http://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo
+sudo wget http://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo && \
 
 # Replace $releasever with 6 in the Maven repository configuration
-sudo sed -i 's/\$releasever/6/g' /etc/yum.repos.d/epel-apache-maven.repo
+sudo sed -i 's/\$releasever/6/g' /etc/yum.repos.d/epel-apache-maven.repo && \
 
 # Install Apache Maven
-sudo yum install -y apache-maven
+sudo yum install -y apache-maven && \
 
 # Update Java version to 11
-sudo update-alternatives --set java $(sudo update-alternatives --list java | grep 'java-11-openjdk.x86_64')
+sudo update-alternatives --set java $(sudo update-alternatives --list java | grep 'java-11-openjdk.x86_64') && \
 
 # Display initial Jenkins admin password
 cat /var/lib/jenkins/secrets/initialAdminPassword
