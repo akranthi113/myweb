12265c2eb986842f3d07b3f787f06cfc26142f0a
akranthi113 <150571677+akranthi113@users.noreply.github.com>
2024-02-12 21:35:40 +0530

    Update install_tomcat.sh

--- install_tomcat.sh
+++ install_tomcat.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 
-# Install Java 1.8 JDK
-sudo yum install -y java-1.8.0-openjdk-devel.x86_64
+# Tomcat version
+TOMCAT_VERSION="9.0.85"  # Adjust this version number as needed
 
-# Download Tomcat latest version (adjust the version number if needed)
-TOMCAT_VERSION=$(curl -s https://archive.apache.org/dist/tomcat/ | grep -oP '(?<=href="v)([0-9]+\.[0-9]+\.[0-9]+)/' | tail -1 | sed 's/\///g')
-TOMCAT_DOWNLOAD_URL="https://archive.apache.org/dist/tomcat/tomcat-${TOMCAT_VERSION}/bin/apache-tomcat-${TOMCAT_VERSION}.tar.gz"
-wget -P /home/ec2-user "${TOMCAT_DOWNLOAD_URL}"
+# Download Tomcat archive
+wget -P /home/ec2-user https://archive.apache.org/dist/tomcat/tomcat-${TOMCAT_VERSION}/bin/apache-tomcat-${TOMCAT_VERSION}.tar.gz
 
-# Extract Tomcat archive with verbose output
-sudo tar -xvf /home/ec2-user/apache-tomcat-${TOMCAT_VERSION}.tar.gz -C /home/ec2-user
+# Extract Tomcat archive
+tar -xvf /home/ec2-user/apache-tomcat-${TOMCAT_VERSION}.tar.gz -C /home/ec2-user/
 
 # Remove lines containing <Valve> with specific patterns from context.xml
-sudo sed -i '/<Valve className="org.apache.catalina.valves.RemoteAddrValve" allow="127\.[0-9]\+\.[0-9]\+\.[0-9]\+|::1|0:0:0:0:0:0:0:1" \/>/d' /home/ec2-user/apache-tomcat-${TOMCAT_VERSION}/webapps/manager/META-INF/context.xml
+sed -i '/<Valve className="org.apache.catalina.valves.RemoteAddrValve" allow="127\.[0-9]\+\.[0-9]\+\.[0-9]\+|::1|0:0:0:0:0:0:0:1" \/>/d' /home/ec2-user/apache-tomcat-${TOMCAT_VERSION}/webapps/manager/META-INF/context.xml
 
 # Add roles and users to tomcat-users.xml
-sudo tee -a /home/ec2-user/apache-tomcat-${TOMCAT_VERSION}/conf/tomcat-users.xml > /dev/null <<EOT
+tee -a /home/ec2-user/apache-tomcat-${TOMCAT_VERSION}/conf/tomcat-users.xml > /dev/null <<EOT
 <role rolename="manager-gui"/>
 <role rolename="manager-script"/>
 <user username="tomcat" password="tomcat" roles="manager-gui,manager-script"/>
