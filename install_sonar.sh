56417255ce5804a5a7c5033acee0e2015be46d91
akranthi113 <150571677+akranthi113@users.noreply.github.com>
2024-02-13 11:05:33 +0530

    Update install_sonar.sh

--- install_sonar.sh
+++ install_sonar.sh
@@ -1,23 +1,16 @@
 #!/bin/bash
 
-# Install Java 8 JDK
-sudo yum install -y java-1.8.0-openjdk-devel.x86_64
+# Change directory to /opt
+cd /opt
+
+# Install Java 8 JDK development package
+yum install -y java-1.8.0-openjdk-devel.x86_64
 
 # Download SonarQube repository configuration
 sudo wget -O /etc/yum.repos.d/sonar.repo http://downloads.sourceforge.net/project/sonar-pkg/rpm/sonar.repo
 
-# Update package index
-sudo yum update -y
+# Install SonarQube package
+sudo yum install sonar -y
 
-# Install SonarQube (adjust the version as needed)
-SONARQUBE_VERSION="9.2"
-sudo yum install -y sonar-${SONARQUBE_VERSION}
-
-# Start SonarQube
-sudo systemctl start sonar
-
-# Enable SonarQube to start on boot
-sudo systemctl enable sonar
-
-# Display status
-echo "SonarQube ${SONARQUBE_VERSION} has been installed and started successfully."
+# Start SonarQube service
+sudo service sonar start
